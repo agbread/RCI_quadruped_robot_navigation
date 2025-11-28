89e2ac5325eaf5b7f2b314a94d4e45a98dda4a95
agbread <jeoug1002@gmail.com>
2025-11-28 22:02:17 +0900

    add hotel stair world and b2 description

--- robots/go2w_description/launch/display.launch.py
+++ robots/go2w_description/launch/display.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='false',
+            default_value='true',
             description='Use sim time if true'),
 
         node_rviz
